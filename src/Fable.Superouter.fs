a94e37e191caaf1d26deb5628c421a6c4773f663
Kristian Mandrup <kmandrup@gmail.com>
2019-02-07 19:59:00 +0100

    convert obj option to simply obj

--- src/Fable.Superouter.fs
+++ src/Fable.Superouter.fs
@@ -10,10 +10,10 @@ let [<Import("URLToken","module")>] uRLToken: URLToken.IExports = jsNative
 let [<Import("Valid","module")>] valid: Valid.IExports = jsNative
 
 type [<AllowNullLiteral>] IExports =
-    abstract tokenizePattern: pattern: string -> obj option
-    abstract tokenizeURL: patternTokens: string * theirURL: string -> obj option
-    abstract ``type``: typename: string * cases: obj option -> unit
-    abstract ``type$safe``: typeName: string * cases: obj option -> obj option
+    abstract tokenizePattern: pattern: string -> obj
+    abstract tokenizeURL: patternTokens: string * theirURL: string -> obj
+    abstract ``type``: typename: string * cases: obj  -> unit
+    abstract ``type$safe``: typeName: string * cases: obj -> obj
 
 module PatternToken =
     let [<Import("Error","module/PatternToken")>] error: Error.IExports = jsNative
@@ -22,40 +22,40 @@ module PatternToken =
 
     type [<AllowNullLiteral>] IExports =
         abstract name: string
-        abstract Part: value: obj option -> unit
-        abstract Path: value: obj option -> unit
-        abstract Variadic: value: obj option -> unit
+        abstract Part: value: obj -> unit
+        abstract Path: value: obj -> unit
+        abstract Variadic: value: obj -> unit
         // { Path, Part, Variadic }
-        abstract fold: obj option -> unit
+        abstract fold: obj -> unit
         abstract groupSpecificity: tokens: string -> unit
         abstract infer: segment: string -> unit
-        abstract isVariadic: o: obj option -> unit
+        abstract isVariadic: o: obj -> unit
         abstract specificity: token: string -> unit
-        abstract toPattern: xs: obj option -> unit
-        abstract toString: x: obj option -> unit
-        abstract validate: [<ParamArray>] args: ResizeArray<obj option> -> unit
-        abstract validateGroup: [<ParamArray>] args: ResizeArray<obj option> -> unit
+        abstract toPattern: xs: obj -> unit
+        abstract toString: x: obj -> unit
+        abstract validate: [<ParamArray>] args: ResizeArray<obj> -> unit
+        abstract validateGroup: [<ParamArray>] args: ResizeArray<obj> -> unit
 
     module Error =
 
         type [<AllowNullLiteral>] IExports =
             abstract name: string
-            abstract DuplicateDef: [<ParamArray>] args: ResizeArray<obj option> -> unit
-            abstract DuplicatePart: [<ParamArray>] args: ResizeArray<obj option> -> unit
-            abstract VariadicCount: [<ParamArray>] args: ResizeArray<obj option> -> unit
-            abstract VariadicPosition: [<ParamArray>] args: ResizeArray<obj option> -> unit
+            abstract DuplicateDef: [<ParamArray>] args: ResizeArray<obj> -> unit
+            abstract DuplicatePart: [<ParamArray>] args: ResizeArray<obj> -> unit
+            abstract VariadicCount: [<ParamArray>] args: ResizeArray<obj> -> unit
+            abstract VariadicPosition: [<ParamArray>] args: ResizeArray<obj> -> unit
 
     module GroupValidations =
 
         type [<AllowNullLiteral>] IExports =
-            abstract duplicateDef: allTokensPairs: obj option -> obj option
+            abstract duplicateDef: allTokensPairs: obj -> obj
 
     module SingleValidations =
 
         type [<AllowNullLiteral>] IExports =
-            abstract duplicatePart: tokens: obj option -> obj option
-            abstract variadicCount: tokens: obj option -> obj option
-            abstract variadicPosition: tokens: obj option -> obj option
+            abstract duplicatePart: tokens: obj -> obj
+            abstract variadicCount: tokens: obj -> obj
+            abstract variadicPosition: tokens: obj -> obj
 
 module URLToken =
     let [<Import("Error","module/URLToken")>] error: Error.IExports = jsNative
@@ -63,51 +63,51 @@ module URLToken =
     let [<Import("validations","module/URLToken")>] validations: Validations.IExports = jsNative
 
     type [<AllowNullLiteral>] IExports =
-        abstract ExcessSegment: segment: obj option -> unit
+        abstract ExcessSegment: segment: obj -> unit
         // { key, value }
-        abstract Part: obj option -> unit
-        abstract Path: value: obj option -> unit
+        abstract Part: obj -> unit
+        abstract Path: value: obj -> unit
         // { expected, actual }
-        abstract Unmatched: obj option -> unit
+        abstract Unmatched: obj -> unit
         // { key, value }
-        abstract Variadic: obj option -> unit
+        abstract Variadic: obj -> unit
         // { Path, Part, Variadic, Unmatched, ExcessSegment }
-        abstract fold: obj option -> unit
-        abstract fromPattern: o: obj option -> unit
-        abstract isVariadic: o: obj option -> unit
-        abstract toArgs: xs: obj option -> unit
-        abstract toString: x: obj option -> unit
-        abstract toURL: xs: obj option -> unit
-        abstract transform: url: string * urlTokens: obj option * patternTokens: obj option -> obj option
-        abstract validate: [<ParamArray>] args: ResizeArray<obj option> -> unit
+        abstract fold: obj -> unit
+        abstract fromPattern: o: obj -> unit
+        abstract isVariadic: o: obj -> unit
+        abstract toArgs: xs: obj -> unit
+        abstract toString: x: obj -> unit
+        abstract toURL: xs: obj -> unit
+        abstract transform: url: string * urlTokens: obj * patternTokens: obj -> obj
+        abstract validate: [<ParamArray>] args: ResizeArray<obj> -> unit
 
     module Error =
 
         type [<AllowNullLiteral>] IExports =
-            abstract ExcessPattern: [<ParamArray>] args: ResizeArray<obj option> -> unit
-            abstract ExcessSegment: [<ParamArray>] args: ResizeArray<obj option> -> unit
-            abstract UnmatchedPaths: [<ParamArray>] args: ResizeArray<obj option> -> unit
+            abstract ExcessPattern: [<ParamArray>] args: ResizeArray<obj> -> unit
+            abstract ExcessSegment: [<ParamArray>] args: ResizeArray<obj> -> unit
+            abstract UnmatchedPaths: [<ParamArray>] args: ResizeArray<obj> -> unit
 
     module Transforms =
 
         type [<AllowNullLiteral>] IExports =
-            abstract collectVariadics: url: string -> obj option
+            abstract collectVariadics: url: string -> obj
 
     module Validations =
 
         type [<AllowNullLiteral>] IExports =
-            abstract excessPatterns: patternTokens: obj option -> obj option
-            abstract excessSegments: patternTokens: obj option -> obj option
-            abstract unmatchedPaths: patternTokens: obj option -> obj option
+            abstract excessPatterns: patternTokens: obj -> obj
+            abstract excessSegments: patternTokens: obj -> obj
+            abstract unmatchedPaths: patternTokens: obj -> obj
 
 module Valid =
 
     type [<AllowNullLiteral>] IExports =
         abstract name: string
-        abstract N: value: obj option -> unit
-        abstract Y: value: obj option -> unit
-        abstract bifold: N: obj option * Y: obj option -> unit
+        abstract N: value: obj -> unit
+        abstract Y: value: obj -> unit
+        abstract bifold: N: obj * Y: obj -> unit
         // { Y, N }
-        abstract fold: obj option -> unit
-        abstract map: f: obj option -> unit
+        abstract fold: obj -> unit
+        abstract map: f: obj -> unit
 
